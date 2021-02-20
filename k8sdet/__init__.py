e3cfa0e58d889814401cd64432b0718ab9e92a51
Ricardo Amaro <ricardo.amaro@gmail.com>
2021-02-20 12:05:33 +0000

    reformat

--- k8sdet/__init__.py
+++ k8sdet/__init__.py
@@ -11,13 +11,17 @@ K8S_API = ApiClient()
 
 K8S_CLIENT_MODELS_URL = "https://github.com/kubernetes-client/python/blob/master/kubernetes/client/models/__init__.py"
 
+
 def from_model(model_name):
     "return class from model"
     model = k8s_model_class_from_name(model_name)
-    class K8sdetModel(K8sObj):
 
+    class K8sdetModel(K8sObj):
         def new(self):
-            _kwargs = {"model": model, "params": {},}
+            _kwargs = {
+                "model": model,
+                "params": {},
+            }
             for k, v in self.kwargs.items():
                 _kwargs["params"][k] = v
             self.kwargs = Dict(_kwargs)
@@ -28,10 +32,13 @@ def from_model(model_name):
 
     return K8sdetModel
 
+
 def set_signature(k8s_model):
     sig = signature(k8s_model)
-    params = [Parameter(param, Parameter.POSITIONAL_OR_KEYWORD, annotation=None)
-            for param, _ in k8s_model.model.attribute_map.items()]
+    params = [
+        Parameter(param, Parameter.POSITIONAL_OR_KEYWORD, annotation=None)
+        for param, _ in k8s_model.model.attribute_map.items()
+    ]
     k8s_model.__signature__ = sig.replace(parameters=params)
 
 
@@ -42,13 +49,13 @@ def k8s_model_class_from_name(model_name):
         raise ValueError(f"Could not load kubernetes-client model '{model_name}'")
     return model_class
 
+
 class K8sObj(BaseObj):
     def new(self):
         self.need(
             "model", f"need kubernetes-client model class from {K8S_CLIENT_MODELS_URL}"
         )
         self.need("params", f"need kubernetes-client model params", istype=dict)
-
 
     def body(self):
         logger.debug("using model params", self.kwargs.params)
